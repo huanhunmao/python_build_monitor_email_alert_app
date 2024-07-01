486b75a457712133413d52eaf2f862154b346f56
Marxu <fuhaojun@52tt.com>
2024-07-01 18:20:53 +0800

    feat: 获取当前的 day 和 time 放到 视频监控帧 上

--- camera_more.py
+++ camera_more.py
@@ -1,5 +1,6 @@
 import cv2
 import streamlit as st
+from datetime import  datetime
 
 st.title('Motion Detector')
 start = st.button('Start Camera')
@@ -16,11 +17,23 @@ if start:
         # 将BGR图像转换为RGB以便Streamlit显示
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        # 在帧上添加文字
-        cv2.putText(img=frame, text='Hello', org=(50,50),
-                    fontFace=cv2.FONT_HERSHEY_PLAIN,color=(20,100,200),
-                    thickness=2,lineType=cv2.LINE_AA,fontScale=2
-                    )
+        # 获取当前时间
+        now = datetime.now()
 
+        # 在帧上添加文字
+        # cv2.putText(img=frame, text='Hello', org=(50,50),
+        #             fontFace=cv2.FONT_HERSHEY_PLAIN,color=(20,100,200),
+        #             thickness=2,lineType=cv2.LINE_AA,fontScale=2
+        #             )
+
+
+        # 获取 day 和 time 添加到 frame
+        cv2.putText(img=frame, text=now.strftime("%A"), org=(30, 80),
+                    fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=3,
+                    color=(255, 255, 255),
+                    thickness=2, lineType=cv2.LINE_AA)
+        cv2.putText(img=frame, text=now.strftime("%H:%M:%S"), org=(30, 140),
+                    fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=3, color=(255, 0, 0),
+                    thickness=2, lineType=cv2.LINE_AA)
         # 在Streamlit应用中显示处理后的帧
         streamlit_image.image(frame)
